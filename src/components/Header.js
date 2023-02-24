fc722a0d1cedf798314a0b23e38a06f45746b73d
Luoxi Pan <l.panpax@gmail.com>
2023-02-24 11:15:40 +1100

    Add React Links to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../styles/Header.css';
+
+import { Link, Router } from 'react-router-dom';
 
 function Header() {
+    const navStyle = {
+        color: 'white',
+    };
+
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <nav>
+            <h3>Logo</h3>
+            <ul className="nav-links">
+                <Link style={navStyle} to="/">
+                    <li>Home</li>
+                </Link>
+
+                <Link style={navStyle} to="shop">
+                    <li>Shop</li>
+                </Link>
+            </ul>
+        </nav>
     );
 }
 
