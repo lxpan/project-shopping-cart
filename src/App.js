b6fdbd4eeb211e24dec4ad439ae40acb2b266cc9
Luoxi Pan <l.panpax@gmail.com>
2023-02-24 11:08:10 +1100

    Fix: include Header within BrowserRouter context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-// import Home from './components/Home';
+import {
+    BrowserRouter, Switch, Routes, Route,
+} from 'react-router-dom';
+import Home from './components/Home';
 import Header from './components/Header';
+import Shop from './components/Shop';
 import RouteSwitch from './RouteSwitch';
 import './App.css';
 
 function App() {
     return (
         <div>
-            <Header />
-            <RouteSwitch />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} exact />
+                    <Route path="/shop" element={<Shop />} exact />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
