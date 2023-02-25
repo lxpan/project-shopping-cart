dde001c2456a64e45af5c783bc0219e4b879b458
Luoxi Pan <l.panpax@gmail.com>
2023-02-25 19:43:16 +1100

    Create callback to add items to cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
@@ -7,13 +7,22 @@ import shopItems from './shopItems';
 import './App.css';
 
 function App() {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState({});
 
     function handleAddToCart(itemId, quantity) {
-        const orderItem = shopItems.filter((item) => item.id === itemId);
+        const itemObj = shopItems.filter((item) => item.id === itemId);
 
-        console.log(itemId, quantity);
+        const itemCartObj = {
+            itemInfo: itemObj,
+            quantity,
+        };
+
+        setCartItems({ ...cartItems, [itemId]: itemCartObj });
     }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
 
     return (
         <div>
