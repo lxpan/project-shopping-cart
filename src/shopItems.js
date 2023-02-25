05130cfbbf0893b3642eebb7edff585073c1c773
Luoxi Pan <l.panpax@gmail.com>
2023-02-25 11:29:19 +1100

    Transfer shop items data into its own file

---
+++ src/shopItems.js
@@ -0,0 +1,50 @@
+import spb123j1 from './assets/images/SPB123J1.png';
+import spb155j1 from './assets/images/SPB155J1.png';
+import spb249j1 from './assets/images/SPB249J1.png';
+import murph from './assets/images/khaki-field-murph.webp';
+import coop from './assets/images/pilot-day-date.webp';
+
+const shopItems = [
+    {
+        name: 'Prospex Land SPB123',
+        alias: 'Alpinist',
+        id: 'SPB123J1',
+        image: spb123j1,
+        cost: 725,
+        brand: 'Seiko',
+    },
+    {
+        name: 'Prospex Land SPB155',
+        alias: 'Baby Alpinist',
+        id: 'SPB155J1',
+        image: spb155j1,
+        cost: 725,
+        brand: 'Seiko',
+    },
+    {
+        name: 'Prospex Land SPB249',
+        alias: 'Alpinist 1959 Deep Lake',
+        id: 'SPB249J1',
+        image: spb249j1,
+        cost: 750,
+        brand: 'Seiko',
+    },
+    {
+        name: 'Khaki Field Auto 38mm',
+        alias: 'Murph',
+        id: 'H70405730',
+        image: murph,
+        cost: 895,
+        brand: 'Hamilton',
+    },
+    {
+        name: 'Khaki Pilot Day-Date',
+        alias: 'Coop',
+        id: 'H64615135',
+        image: coop,
+        cost: 995,
+        brand: 'Hamilton',
+    },
+];
+
+export default shopItems;
