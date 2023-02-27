676c46a3ecddeffc642f23f01945c06e97180f11
Luoxi Pan <l.panpax@gmail.com>
2023-02-27 12:31:27 +1100

    Add test for quantity buttons in cart

--- src/components/tests/ShoppingCart.test.js
+++ src/components/tests/ShoppingCart.test.js
@@ -53,3 +53,36 @@ describe('Shopping cart renders cart items', () => {
         expect(image).toHaveAttribute('src', 'rolex-explorer.png');
     });
 });
+
+describe('Testing increment and decrement buttons', () => {
+    let counter;
+    let incrementBtn;
+    let decrementBtn;
+
+    beforeEach(async () => {
+        render(<App />);
+        counter = await waitFor(() => screen.getByRole('textbox'));
+        incrementBtn = await waitFor(() => screen.getByText('+'));
+        decrementBtn = await waitFor(() => screen.getByText('-'));
+    });
+
+    test('Quantity is incremented on increment button click', async () => {
+        act(() => UserEvent.click(incrementBtn));
+
+        expect(counter.value).toEqual('Qty: 2');
+    });
+
+    test('Quantity is decremented on decrement button click', async () => {
+        act(() => UserEvent.click(incrementBtn)); // 1 -> 2
+        act(() => UserEvent.click(decrementBtn)); // 2 -> 1
+
+        expect(counter.value).toEqual('Qty: 1');
+    });
+
+    test('Decrement button does not cause quantity to go below zero', async () => {
+        act(() => UserEvent.click(decrementBtn)); // 1 -> 0
+        act(() => UserEvent.click(decrementBtn)); // 0
+
+        expect(counter.value).toEqual('Qty: 0');
+    });
+});
