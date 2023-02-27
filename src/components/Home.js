736c92a8e69099ffe427f375573d14e1abe1a504
Luoxi Pan <l.panpax@gmail.com>
2023-02-27 18:40:31 +1100

    Add tri-grid "nature & watch" background to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import '../styles/Home.css';
 
+import backgroundLeft from '../assets/backgrounds/markus-spiske.jpg';
+import backgroundMiddle from '../assets/backgrounds/woodland.jpg';
+import backgroundRight from '../assets/backgrounds/blue-lake.jpg';
+import watchLeft from '../assets/images/SPB123J1.png';
+import watchMiddle from '../assets/images/SPB155J1.png';
+import watchRight from '../assets/images/SPB249J1.png';
+
+const style = {
+    left: {
+        backgroundImage: `url(${backgroundLeft})`,
+        backgroundSize: 'cover',
+    },
+    middle: {
+        backgroundImage: `url(${backgroundMiddle})`,
+        backgroundSize: 'cover',
+    },
+    right: {
+        backgroundImage: `url(${backgroundRight})`,
+        backgroundSize: 'cover',
+    },
+};
+
 function Home() {
     return (
         <div className="home-container">
-            <h1>
+            <div className="home-left" style={style.left}>
+                <img src={watchLeft} alt="" srcSet="" />
+                <div className="attribution">Photo by Markus Spiske</div>
+            </div>
+            <div className="home-middle" style={style.middle}>
+                <img src={watchMiddle} alt="" srcSet="" />
+                <div className="attribution">Photo by Lukasz Szmigiel</div>
+            </div>
+            <div className="home-right" style={style.right}>
+                <img src={watchRight} alt="" srcSet="" />
+                <div className="attribution">Photo by Yoal Desurmont</div>
+            </div>
+            {/* <h1>
                 <em>The Alpine Collection</em>
-            </h1>
+            </h1> */}
         </div>
     );
 }
